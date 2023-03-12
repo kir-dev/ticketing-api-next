89b4a1b4bc124c062e4ff5767708e5fc45585be5
Sámuel Fekete <feketesamu@gmail.com>
2023-03-12 13:50:23 +0100

    params excercise

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(name?: string, day?: string): string {
+    return `Hello, ${name || 'World'}, have a nice ${day || 'day'}!`;
   }
 }
